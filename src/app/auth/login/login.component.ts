5164e03b1631a9d1c9e468f0f0ab3e4bcf5a80b8
Mayukh Datta <mayukh5741@gmail.com>
2021-05-09 00:48:19 +0530

    modified login form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  logInForm: FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
+    // creating reactive signup form
+    this.logInForm = new FormGroup({
+      'email': new FormControl('', [Validators.required, Validators.email]),
+      'password': new FormControl('', [Validators.required, Validators.minLength(8)])
+    });
+  }
+
+  // method binded to form ngSubmit event
+  onLogIn() {
+    console.log(this.logInForm);
+    //console.log(formData.form.value);
   }
 
 }
